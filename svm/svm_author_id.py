ac4431adc03076ff49480322d7798391234e8910
Akshaya Rane <akshaya.uky@gmail.com>
2018-01-10 15:53:47 -0800

    test

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,30 +1,38 @@
-#!/usr/bin/python
-
-""" 
-    This is the code to accompany the Lesson 2 (SVM) mini-project.
-
-    Use a SVM to identify emails from the Enron corpus by their authors:    
-    Sara has label 0
-    Chris has label 1
-"""
-    
 import sys
-from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+sys.path.append("/home/akshaya/udacity-ml-course/ud120-projects/tools/")
+sys.path.append('/home/akshaya/udacity-ml-course/ud120-projects/choose_your_own')
+sys.path.append('/home/akshaya/udacity-ml-course/ud120-projects/svm')
+
+import os
+os.chdir('/home/akshaya/udacity-ml-course/ud120-projects/svm')
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+
+import sys
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+import pylab as pl
+
+from sklearn.metrics import accuracy_score
 
 
+features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
-#########################################################
-### your code goes here ###
+########################## SVM #################################
+from sklearn.svm import SVC
 
-#########################################################
+def submitAccuracy():
+    return accuracy_score(pred, labels_test)
 
+clf = SVC(kernel="linear")
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+#print accuracy_score(pred, labels_test)
+
+prettyPicture(clf, features_test, labels_test)
+plt.show()

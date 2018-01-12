5667a0eb218ea3cf9886823678a0163876fb4c62
Akshaya Rane <akshaya.uky@gmail.com>
2018-01-11 19:27:58 -0800

    Complete code

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,38 +1,52 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+
 import sys
 sys.path.append("/home/akshaya/udacity-ml-course/ud120-projects/tools/")
-sys.path.append('/home/akshaya/udacity-ml-course/ud120-projects/choose_your_own')
-sys.path.append('/home/akshaya/udacity-ml-course/ud120-projects/svm')
-
-import os
-os.chdir('/home/akshaya/udacity-ml-course/ud120-projects/svm')
-
-
-from class_vis import prettyPicture
-from prep_terrain_data import makeTerrainData
-
-import sys
-import matplotlib.pyplot as plt
-import copy
-import numpy as np
-import pylab as pl
-
+from time import time
+from email_preprocess import preprocess
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 
-features_train, labels_train, features_test, labels_test = makeTerrainData()
-
+features_train, features_test, labels_train, labels_test = preprocess()
+#To speed up the algorithm by considering only 1% of the total data
+##features_train = features_train[:len(features_train)/100] 
+##labels_train = labels_train[:len(labels_train)/100] 
 
 ########################## SVM #################################
-from sklearn.svm import SVC
+
 
 def submitAccuracy():
     return accuracy_score(pred, labels_test)
 
-clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf", C=10000.0)
+
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
 pred = clf.predict(features_test)
+#to predict the label class of element 20 in the test sample.
+##pred = clf.predict(features_test)[20]
+##print pred
+print "predicting time:", round(time()-t1, 3), "s"
+t2 = time()
+print "accuracy: ", accuracy_score(pred, labels_test)
+print "scoring time:", round(time()-t2, 3), "s"
 
-#print accuracy_score(pred, labels_test)
+## To get number of predicted emails written by Chris.  Ans: 877
+count = 0
+for i in range(1,1700):
+    pred = clf.predict(features_test)[i]
+    i = i + 1
+    count = count + pred
+print count
 
-prettyPicture(clf, features_test, labels_test)
-plt.show()
+#################################################################
